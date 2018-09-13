333e7b3bc9e873b7274f792380bfdb465bb25034
Alex Koller <alex.koller@r3.com>
2018-09-13 11:28:49 +0100

    Better tests

--- integration-tests/src/test/kotlin/net/corda/derivativestradingnetwork/integrationTests/EndToEndTest.kt
+++ integration-tests/src/test/kotlin/net/corda/derivativestradingnetwork/integrationTests/EndToEndTest.kt
@@ -1,33 +1,14 @@
 package net.corda.derivativestradingnetwork.integrationTests
 
-import net.corda.client.rpc.CordaRPCClient
-import net.corda.core.CordaRuntimeException
-import net.corda.core.contracts.FungibleAsset
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.internal.errors.AddressBindingException
-import net.corda.core.messaging.startFlow
-import net.corda.core.messaging.vaultQueryBy
-import net.corda.core.utilities.NetworkHostAndPort
-import net.corda.core.utilities.OpaqueBytes
-import net.corda.core.utilities.getOrThrow
-import net.corda.finance.DOLLARS
-import net.corda.finance.contracts.asset.Cash
-import net.corda.finance.flows.CashIssueFlow
-import net.corda.node.services.Permissions
-import net.corda.testing.core.ALICE_NAME
-import net.corda.testing.core.DUMMY_BANK_A_NAME
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.driver.*
-import net.corda.testing.node.User
-import org.assertj.core.api.Assertions
 import org.junit.Test
-import java.net.InetSocketAddress
-import java.net.ServerSocket
 import kotlin.test.assertEquals
 
 class EndToEndTest {
 
-    fun setUpEnvironmentAndRunTest(test : (DriverDSL, BnoNode, MemberNode)->Unit) {
+    fun setUpEnvironmentAndRunTest(test : (DriverDSL, BnoNode, MemberNode, MemberNode, MemberNode)->Unit) {
         driver(DriverParameters(isDebug = true, startNodesInProcess = true,
                 extraCordappPackagesToScan = listOf(
                         "net.corda.businessnetworks.membership.member.service",
@@ -38,19 +19,23 @@ class EndToEndTest {
 
 
 
-            // ----- bno, customer, bank, datastore and attester start nodes -------
+            // ----- bno, client, dealer, ccp start nodes -------
             val bno = BnoNode(this, TestIdentity(CordaX500Name("BNO", "New York", "US")),false)
             val client = MemberNode(this, TestIdentity(CordaX500Name("Client", "", "US")), false)
+            val dealer = MemberNode(this, TestIdentity(CordaX500Name("Dealer", "", "US")), false)
+            val ccp = MemberNode(this, TestIdentity(CordaX500Name("CCP", "", "US")), false)
 
-            listOf(bno, client).map { it.startCoreAsync() }.map { it.waitForCoreToStart() }.map { it.startWebAsync() }.map { it.waitForWebToStart() }
+            listOf(bno, client, dealer, ccp).map { it.startCoreAsync() }.map { it.waitForCoreToStart() }.map { it.startWebAsync() }.map { it.waitForWebToStart() }
 
             //confirm all the nodes are on the network
             bno.confirmNodeIsOnTheNetwork()
             client.confirmNodeIsOnTheNetwork()
+            dealer.confirmNodeIsOnTheNetwork()
+            ccp.confirmNodeIsOnTheNetwork()
 
 
             //run the test
-            test(this, bno, client)
+            test(this, bno, client, dealer, ccp)
 
         }
     }
@@ -58,12 +43,25 @@ class EndToEndTest {
 
     @Test
     fun `Nodes can ask for and get membership`() {
-        setUpEnvironmentAndRunTest { _, bno, client ->
-            //ask for membership
+        setUpEnvironmentAndRunTest { _, bno, client, dealer, ccp ->
+            //at the beginning there are no members
             assertEquals(0,bno.getMembershipStates().size)
-            client.askForMembership("Some Customer","meh")
+
+            //client asks for membership
+            client.askForMembership("Client","Alternative Name")
             bno.approveMembership(client.testIdentity.party)
             assertEquals(1,bno.getMembershipStates().size)
+
+            //dealer asks for membership
+            dealer.askForMembership("Dealer","Alternative Name")
+            bno.approveMembership(dealer.testIdentity.party)
+            assertEquals(2,bno.getMembershipStates().size)
+
+            //ccp asks for membership
+            ccp.askForMembership("CCP","Alternative Name")
+            bno.approveMembership(ccp.testIdentity.party)
+            assertEquals(3,bno.getMembershipStates().size)
+
         }
     }
 

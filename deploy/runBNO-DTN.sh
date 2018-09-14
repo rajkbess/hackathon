646e3244fab884085985997fc18e04cac9906bd3
Ian Morgan <ian.morgan@r3.com>
2018-09-14 11:07:58 +0100

    Fixing bugs in custom run scripts #2

--- deploy/runBNO-DTN.sh
+++ deploy/runBNO-DTN.sh
@@ -7,7 +7,7 @@ echo "Killing existing $NODENAME processes"
 for pid in $(ps -ef | grep "java" | grep "$NODENAME" | awk '{print $2}'); do kill -9  $pid; done
 
 echo "Starting Node $NODENAME"
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=BNO-DTN -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010 -javaagent:drivers/jolokia-jvm-1.3.7-agent.jar=port=7010,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=BNO-DTN -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010 -javaagent:drivers/jolokia-jvm-1.3.7-agent.jar=port=7010,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell &
 
 echo "Waiting for Node $NODENAME to start"
 while ! echo exit | nc localhost 10012; do sleep 10 && echo "waiting on $NODENAME"; done
@@ -16,6 +16,6 @@ while ! echo exit | nc localhost 10012; do sleep 10 && echo "waiting on $NODENAM
 sleep 10
 
 echo "Starting web server $NODENAME"
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=BNO-DTN -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5011 -jar corda-webserver.jar
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=BNO-DTN -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5011 -jar corda-webserver.jar &
 
 cd .
--- deploy/runDEALER-D01.sh
+++ deploy/runDEALER-D01.sh
@@ -7,7 +7,7 @@ echo "Killing existing $NODENAME processes"
 for pid in $(ps -ef | grep "java" | grep "$NODENAME" | awk '{print $2}'); do kill -9  $pid; done
 
 echo "Starting Node $NODENAME"
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=DEALER-D01 -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5007 -javaagent:drivers/jolokia-jvm-1.3.7-agent.jar=port=7007,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=DEALER-D01 -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5007 -javaagent:drivers/jolokia-jvm-1.3.7-agent.jar=port=7007,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell &
 
 echo "Waiting for Node $NODENAME to start"
 while ! echo exit | nc localhost 10009; do sleep 10 && echo "waiting on $NODENAME"; done
@@ -16,6 +16,6 @@ while ! echo exit | nc localhost 10009; do sleep 10 && echo "waiting on $NODENAM
 sleep 10
 
 echo "Starting web server $NODENAME"
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=DEALER-D01 -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5008 -jar corda-webserver.jar
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=DEALER-D01 -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5008 -jar corda-webserver.jar &
 
 cd .

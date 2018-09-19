9df88c6716ba4fcd572113141a4d15e091e162e0
Ian Morgan <ian.morgan@r3.com>
2018-09-19 16:03:24 +0100

    trying to stop "address already in use" problem with jolokia

--- deploy/runNode.sh
+++ deploy/runNode.sh
@@ -22,7 +22,7 @@ echo "Killing existing $NODENAME processes"
 for pid in $(ps -ef | grep "java" | grep "$NODENAME" | awk '{print $2}'); do kill -9  $pid; done
 
 echo "Starting Node $NODENAME"
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=$NODENAME -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$((BASEPORT + 5)) -javaagent:drivers/jolokia-jvm-1.6.0-agent.jar=port=7005,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell &
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Dname=$NODENAME -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$((BASEPORT + 5)) -javaagent:drivers/jolokia-jvm-1.6.0-agent.jar=port=$((BASEPORT + 7)),logHandlerClass=net.corda.node.JolokiaSlf4jAdapter -jar corda.jar --no-local-shell &
 
 echo "Waiting for Node $NODENAME to start"
 while ! echo exit | nc localhost $((BASEPORT + 1)); do sleep 10 && echo "waiting on $NODENAME"; done

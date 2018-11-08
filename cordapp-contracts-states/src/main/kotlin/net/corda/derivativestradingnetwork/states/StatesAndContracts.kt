a0415ec8f53c04b27f4d82c1af0dbb3f038552d4
Alex Koller <alex.koller@r3.com>
2018-11-08 23:12:57 +0000

    Code volume 2

--- cordapp-contracts-states/src/main/kotlin/net/corda/derivativestradingnetwork/states/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/net/corda/derivativestradingnetwork/states/StatesAndContracts.kt
@@ -3,5 +3,35 @@ package net.corda.derivativestradingnetwork.states
 import net.corda.core.contracts.*
 import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
+import java.util.*
 
 
+class MoneyToken : Contract {
+    companion object {
+        val CONTRACT_NAME = "net.corda.derivativestradingnetwork.states.MoneyToken"
+    }
+
+    open class Commands : CommandData {
+        class Issue : Commands()
+        class Transfer : Commands()
+        class Redeem : Commands()
+    }
+
+    override fun verify(tx : LedgerTransaction) {
+       //@todo put your contract checks here
+        //1) amount can't be over 1000
+        //2) if between 150 and 1000 then one of the signers has to be the amlauthority
+    }
+
+
+    data class State(
+            val amount : Long,
+            val currency : Currency,
+            val issuer : Party,
+            val holder : Party,
+            val amlAuthority : Party) : ContractState {
+
+
+        override val participants = listOf(holder)
+    }
+}
--- cordapp/src/main/kotlin/net/corda/derivativestradingnetwork/flow/TokenIssuance.kt
+++ cordapp/src/main/kotlin/net/corda/derivativestradingnetwork/flow/TokenIssuance.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 @InitiatingFlow
 @StartableByRPC
-class UserIssuanceRequestFlow(val amount : Long, val currency : Currency, val ourBank : Party) : FlowLogic<SignedTransaction>() {
+class UserIssuanceRequestFlow(val amount : Long, val currency : Currency, val ourBank : Party) : FlowLogic<Unit>() {
 
     companion object {
         object SENDING_REQUEST_TO_BANK : ProgressTracker.Step("Sending request to bank")
@@ -25,21 +25,19 @@ class UserIssuanceRequestFlow(val amount : Long, val currency : Currency, val ou
     override val progressTracker = tracker()
 
     @Suspendable
-    override fun call(): SignedTransaction {
+    override fun call()  {
         logger.info("Starting UserIssuanceRequestFlow")
         progressTracker.currentStep = SENDING_REQUEST_TO_BANK
 
         val session = initiateFlow(ourBank)
         session.send(IssuanceRequest(amount, currency))
-
-        throw UnsupportedOperationException()
     }
 }
 
 @InitiatedBy(UserIssuanceRequestFlow::class)
 class UserIssuanceRequestResponderFlow(val flowSession : FlowSession) : FlowLogic<SignedTransaction>() {
 
-
+    @Suspendable
     override fun call(): SignedTransaction {
         logger.info("Initiated by user")
 
@@ -47,6 +45,56 @@ class UserIssuanceRequestResponderFlow(val flowSession : FlowSession) : FlowLogi
 
         logger.info("Received request $requestFromClient")
 
+        val amount = requestFromClient.amount
+        val currency = requestFromClient.currency
+
+        return subFlow(TokenIssuanceFlow(amount, currency, flowSession.counterparty))
+    }
+
+
+}
+
+@InitiatingFlow
+@StartableByRPC
+class TokenIssuanceFlow(val amount : Long, val currency : Currency, val user : Party) : FlowLogic<SignedTransaction>() {
+
+    companion object {
+        object PROPOSING_TRANSACTION_TO_ISSUER : ProgressTracker.Step("Proposing transaction to issuer")
+
+
+        fun tracker() = ProgressTracker(
+                PROPOSING_TRANSACTION_TO_ISSUER
+        )
+    }
+
+    override val progressTracker = tracker()
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        logger.info("Starting TokenIssuanceFlow")
+        progressTracker.currentStep = PROPOSING_TRANSACTION_TO_ISSUER
+
+        val issuer = getIssuer()
+        val sessionWithIssuer = initiateFlow(issuer)
+
+        //@todo create the transaction here with the output state of the money token, the holder being the bank
+        //@todo and when that's signed by the ECB and notarized then invoke another flow that will change the holder from the bank to the user
+
         throw UnsupportedOperationException()
+    }
+
+    @Suspendable
+    private fun getIssuer() : Party {
+        return serviceHub.networkMapCache.allNodes.map { it.legalIdentities.first() }.filter { it.name.organisation.contains("issuer", true) }.single()
+    }
+}
+
+@InitiatedBy(TokenIssuanceFlow::class)
+class TokenIssuanceResponderFlow(val flowSession : FlowSession) : FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        //@todo this is the point where the issuer (ECB) has to sign the transaction coming from the commercial bank. Speak to Cais.
+        throw java.lang.UnsupportedOperationException("Talk to Cais")
     }
 }

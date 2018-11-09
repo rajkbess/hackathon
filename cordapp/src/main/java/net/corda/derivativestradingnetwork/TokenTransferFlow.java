ad729e18649438c484eeb8a96919fc698d8785fb
joeldudleyr3 <joel.dudley@r3.com>
2018-11-09 10:18:19 +0000

    Implements token transfer with no AML.

--- cordapp/src/main/java/net/corda/derivativestradingnetwork/TokenTransferFlow.java
+++ cordapp/src/main/java/net/corda/derivativestradingnetwork/TokenTransferFlow.java
@@ -1,5 +1,78 @@
 package net.corda.derivativestradingnetwork;
 
-public class TokenTransferFlow {
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.flows.*;
+import net.corda.core.identity.Party;
+import net.corda.core.node.services.NetworkMapCache;
+import net.corda.core.node.services.VaultService;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+import net.corda.derivativestradingnetwork.states.MoneyToken;
 
+import java.util.Currency;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@InitiatingFlow
+@StartableByRPC
+public class TokenTransferFlow extends FlowLogic<Void> {
+    private final Currency currency;
+    private final int amount;
+    private final Party newOwner;
+
+    private final ProgressTracker progressTracker = new ProgressTracker();
+
+    public TokenTransferFlow(Currency currency, int amount, Party newOwner) {
+        this.currency = currency;
+        this.amount = amount;
+        this.newOwner = newOwner;
+    }
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        NetworkMapCache networkMap = getServiceHub().getNetworkMapCache();
+        Party notary = networkMap.getNotaryIdentities().get(0);
+
+        VaultService vault = getServiceHub().getVaultService();
+        List<StateAndRef<MoneyToken.State>> stateAndRefs =
+                vault.queryBy(MoneyToken.State.class).getStates();
+
+        List<StateAndRef<MoneyToken.State>> matchingStates = stateAndRefs.stream().filter(stateAndRef -> {
+            MoneyToken.State state = stateAndRef.getState().getData();
+            Boolean isCorrectAmount = state.getAmount() == amount;
+            Boolean isCorrectCurrency = state.getCurrency().equals(currency);
+            return isCorrectAmount && isCorrectCurrency;
+        }).collect(Collectors.toList());
+
+        if (matchingStates.isEmpty())
+            throw new IllegalArgumentException("No matching token to transfer.");
+        StateAndRef<MoneyToken.State> inputStateAndRef = matchingStates.get(0);
+        MoneyToken.State inputState = inputStateAndRef.getState().getData();
+
+        MoneyToken.State outputToken =
+                new MoneyToken.State(amount, currency, inputState.getIssuer(), newOwner, null);
+        CommandData transferCommand = new MoneyToken.Commands.Transfer();
+
+        TransactionBuilder txBuilder = new TransactionBuilder(notary);
+        txBuilder.addInputState(inputStateAndRef);
+        txBuilder.addOutputState(outputToken, MoneyToken.Companion.getCONTRACT_NAME());
+        txBuilder.addCommand(transferCommand, getOurIdentity().getOwningKey());
+
+        txBuilder.verify(getServiceHub());
+
+        SignedTransaction stx = getServiceHub().signInitialTransaction(txBuilder);
+
+        subFlow(new FinalityFlow(stx));
+
+        return null;
+    }
 }
--- cordapp/src/test/kotlin/net/corda/FlowTests.kt
+++ cordapp/src/test/kotlin/net/corda/FlowTests.kt
@@ -3,9 +3,9 @@ package net.corda
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
+import net.corda.derivativestradingnetwork.TokenTransferFlow
 import net.corda.derivativestradingnetwork.flow.UserIssuanceRequestFlow
 import net.corda.derivativestradingnetwork.states.MoneyToken
-import net.corda.finance.DOLLARS
 import net.corda.finance.EUR
 import net.corda.testing.core.singleIdentity
 import net.corda.testing.node.MockNetwork
@@ -18,12 +18,14 @@ import kotlin.test.assertEquals
 class FlowTests {
     private val network = MockNetwork(listOf("net.corda"))
 
-    private val user = network.createNode()
+    private val userOne = network.createNode()
+    private val userTwo = network.createNode()
     private val commercialBank = network.createNode()
     private val centralBankName = CordaX500Name("issuer", "Frankfurt", "DE")
     private val centralBank = network.createNode(MockNodeParameters().withLegalName(centralBankName))
 
-    private val userParty = user.info.singleIdentity()
+    private val userOneParty = userOne.info.singleIdentity()
+    private val userTwoParty = userTwo.info.singleIdentity()
     private val commercialBankParty = commercialBank.info.singleIdentity()
     private val centralBankParty = centralBank.info.singleIdentity()
 
@@ -36,18 +38,36 @@ class FlowTests {
     @Test
     fun `golden path test`() {
         val flow = UserIssuanceRequestFlow(100, EUR, commercialBankParty)
-        val future = user.startFlow(flow)
+        val future = userOne.startFlow(flow)
         network.runNetwork()
         future.getOrThrow()
-        network.waitQuiescent()
 
-        user.transaction {
-            val moneyStates = user.services.vaultService.queryBy<MoneyToken.State>().states
+        userOne.transaction {
+            val moneyStates = userOne.services.vaultService.queryBy<MoneyToken.State>().states
             assertEquals(1, moneyStates.size)
             val moneyState = moneyStates.single().state.data
             assertEquals(moneyState.amount, 100)
             assertEquals(moneyState.currency, EUR)
-            assertEquals(moneyState.holder, userParty)
+            assertEquals(moneyState.holder, userOneParty)
+        }
+
+        val flow2 = TokenTransferFlow(EUR, 100, userTwoParty)
+        val future2 = userOne.startFlow(flow2)
+        network.runNetwork()
+        future2.getOrThrow()
+
+        userOne.transaction {
+            val moneyStates = userOne.services.vaultService.queryBy<MoneyToken.State>().states
+            assertEquals(0, moneyStates.size)
+        }
+
+        userTwo.transaction {
+            val moneyStates = userTwo.services.vaultService.queryBy<MoneyToken.State>().states
+            assertEquals(1, moneyStates.size)
+            val moneyState = moneyStates.single().state.data
+            assertEquals(moneyState.amount, 100)
+            assertEquals(moneyState.currency, EUR)
+            assertEquals(moneyState.holder, userTwoParty)
         }
     }
 }
